16b1c9fbe491d64ae633973b9c1c5d765a11f845
Tomasz Blazejowski <tomasz.blazejowski@gmail.com>
2019-07-24 11:00:42 +0200

    added FileLoader class that creates a HashMap with words from input file

---
+++ src/main/java/pl/falcor/FileLoader.java
@@ -0,0 +1,37 @@
+package pl.falcor;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+class FileLoader {
+
+    private final String file;
+    private HashMap<String, Integer> wordsMap = new HashMap<>();
+
+    FileLoader(String file) {
+        this.file = file;
+    }
+
+    HashMap<String, Integer> readFromFile() {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String currentLine = reader.readLine().toLowerCase();
+            while (currentLine != null) {
+                String[] wordsArray = currentLine.replaceAll("[^a-zA-Z ]", "").split("\\s+");
+                for (String s : wordsArray) {
+                    if (wordsMap.containsKey(s)) {
+                        wordsMap.put(s, wordsMap.get(s) + 1);
+                    } else {
+                        wordsMap.put(s, 1);
+                    }
+                }
+                currentLine = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return wordsMap;
+    }
+}
