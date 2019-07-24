2e5305af8057fafef0cee9157fe911d988747bb6
Tomasz Blazejowski <tomasz.blazejowski@gmail.com>
2019-07-24 13:54:43 +0200

    added VowelsStats class to hold values needed for final stats

--- src/main/java/pl/falcor/VowelsMap.java
+++ src/main/java/pl/falcor/VowelsMap.java
@@ -1,31 +1,36 @@
 package pl.falcor;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.HashSet;
 
 class VowelsMap {
 
     private HashMap<String, Integer> wordsMap;
-    private HashMap<VowelsWithLength, Integer> vowelsMap = new HashMap<>();
+    private HashMap<VowelsWithLength, VowelsStats> vowelsMap = new HashMap<>();
 
     VowelsMap(HashMap<String, Integer> wordsMap) {
         this.wordsMap = wordsMap;
     }
 
-    HashMap<VowelsWithLength, Integer> createVowelsMap() {
+    HashMap<VowelsWithLength, VowelsStats> createVowelsMap() {
         for (String word : wordsMap.keySet()) {
             HashSet<Character> vowelsInWord = new HashSet<>();
+            int counter = 0;
             for (int i = 0; i < word.length(); i++) {
                 if (VowelsSet.VOWELS_SET.contains(word.charAt(i))) {
                     vowelsInWord.add(word.charAt(i));
+                    counter++;
                 }
             }
             VowelsWithLength vowelsWithLength = new VowelsWithLength(vowelsInWord, word.length());
             if (vowelsMap.containsKey(vowelsWithLength)) {
-                vowelsMap.put(vowelsWithLength, vowelsMap.get(vowelsWithLength) + 1);
+                vowelsMap.get(vowelsWithLength).increaseOccurrence();
+                vowelsMap.get(vowelsWithLength).setSum(counter);
             } else {
-                vowelsMap.put(vowelsWithLength, 1);
+                vowelsMap.put(vowelsWithLength, new VowelsStats(new BigDecimal(1), new BigDecimal(counter)));
             }
+            counter = 0;
         }
         return vowelsMap;
     }
---
+++ src/main/java/pl/falcor/VowelsStats.java
@@ -0,0 +1,27 @@
+package pl.falcor;
+
+import java.math.BigDecimal;
+
+public class VowelsStats {
+
+    private BigDecimal occurrence;
+    private BigDecimal sum;
+
+    VowelsStats(BigDecimal occurrence, BigDecimal sum) {
+        this.occurrence = occurrence;
+        this.sum = sum;
+    }
+
+    void increaseOccurrence() {
+        this.occurrence = this.occurrence.add(new BigDecimal(1));
+    }
+
+    void setSum(int sum) {
+        this.sum = this.sum.add(new BigDecimal(sum));
+    }
+
+    @Override
+    public String toString() {
+        return sum.divide(occurrence).toString();
+    }
+}
