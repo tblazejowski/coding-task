85c315bd304882cb978ad683374132022eb7ca31
Tomasz Blazejowski <tomasz.blazejowski@gmail.com>
2019-07-24 15:12:54 +0200

    added readme file and passing of input path as argument

---
+++ README.md
@@ -0,0 +1,10 @@
+#### Coding task solution
+
+##### 1. used technology satck
+ - Java 8
+ - Maven 3.5.3
+ 
+##### comments
+
+- time dedicated nearly 5 hours
+- next step would be definitely adding more tests  :)
--- src/main/java/pl/falcor/App.java
+++ src/main/java/pl/falcor/App.java
@@ -2,9 +2,13 @@ package pl.falcor;
 
 class App {
     public static void main(String[] args) {
-        FileLoader fileLoader = new FileLoader("src/test/resources/test1.txt");
-        VowelsMap vowelsMap = new VowelsMap(fileLoader.readFromFile());
-        ResultWriter resultWriter = new ResultWriter("output.txt");
-        resultWriter.writeResultToFile(vowelsMap.getStats().toString());
+        if (args.length != 1) {
+            System.err.println("To execute please provide absolute or relative file path as one argument");
+        } else {
+            FileLoader fileLoader = new FileLoader(args[0]);
+            VowelsMap vowelsMap = new VowelsMap(fileLoader.readFromFile());
+            ResultWriter resultWriter = new ResultWriter("output.txt");
+            resultWriter.writeResultToFile(vowelsMap.getStats().toString());
+        }
     }
 }
