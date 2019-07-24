b04ad454bad403853420b8bd476be60ab37823cc
Tomasz Blazejowski <tomasz.blazejowski@gmail.com>
2019-07-24 14:52:53 +0200

    removed output file with results added to repo by accident

--- src/main/java/pl/falcor/App.java
+++ src/main/java/pl/falcor/App.java
@@ -4,7 +4,7 @@ class App {
     public static void main(String[] args) {
         FileLoader fileLoader = new FileLoader("src/test/resources/test1.txt");
         VowelsMap vowelsMap = new VowelsMap(fileLoader.readFromFile());
-        ResultWriter resultWriter = new ResultWriter("./output.txt");
+        ResultWriter resultWriter = new ResultWriter("output.txt");
         resultWriter.writeResultToFile(vowelsMap.getStats().toString());
     }
 }
